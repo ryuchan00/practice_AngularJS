d2d676280158046b669d45d18e557145a040fd8d
ryuchun00 <leokun0210@gmail.com>
2018-02-24 01:36:59 +0900

    MVCの使い方がわからないので、コメント

--- 2/scripts/controller.js
+++ 2/scripts/controller.js
@@ -1,1 +1,9 @@
-angular.module('myApp', []).controller('MyController', function($scope) {$scope.msg = 'こんにちは、AngularJS！'});
+// モジュールを定義する。ここでは`myApp`というモジュールを定義した。
+angular.module('myApp', [])
+    // コントローラーを定義する。moducleメソッドの戻り値はモジュール(Moduleオブジェクト)です。
+    // ここでは(myApp モジュールの)controllerメソッドを利用して、新たにMyControllerコントローラーを定義しいてます。
+    .controller('MyController', function ($scope) {
+        // スコープオブジェクト($scope)とは、テンプレート(HTML)とJSによるモデルとを橋渡しするためのオブジェクトです。
+        // AngularJSの世界では、テンプレートで利用する値や挙動は、スコープを介して受け取るのが基本です。
+        $scope.msg = 'こんにちは、AngularJS！'
+    });
