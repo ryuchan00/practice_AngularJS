ca1f9effc4e68bb3f326012e86f30a6ed9ce6361
ryuchun00 <leokun0210@gmail.com>
2018-05-14 22:40:01 +0900

    $logを出力

--- udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
+++ udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
@@ -1,11 +1,6 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function ($scope) {
+myApp.controller('mainController', function ($scope, $log) {
+    console.log($log);
     console.log($scope);
 });
-
-let searchPeople = function ($scope, lastName, height, age, hobby) {
-    return '山田　太郎'
-};
-
-console.log(angular.injector().annotate(searchPeople));
