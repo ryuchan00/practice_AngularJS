5b5702b2eaf55227440e89ce94491d9e6896c496
ryuchun00 <leokun0210@gmail.com>
2018-05-14 23:36:49 +0900

    moduleを入れてみた

--- udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
+++ udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
@@ -1,5 +1,6 @@
-let myApp = angular.module('myApp', ['ngMessages']); // moduleの第二引数には依存するモジュールを入れる
+let myApp = angular.module('myApp', ['ngMessages', 'ngResource']); // moduleの第二引数には依存するモジュールを入れる
 // この場合は、'ngMessages'というモジュールに依存してるよ!!とAngularJSに教えている
 
-myApp.controller('mainController', function () {
+myApp.controller('mainController', function ($scope, $log, $filter, $resource) {
+    console.log($resource);
 });
