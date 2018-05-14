74ed19d4174cf56be2b1f7e810bfd558511c3993
ryuchun00 <leokun0210@gmail.com>
2018-05-14 23:43:06 +0900

    JavaScriptの配列の中のfunctionを呼び出してみた

--- udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
+++ udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
@@ -1,6 +1,6 @@
-let myApp = angular.module('myApp', ['ngMessages', 'ngResource']); // moduleの第二引数には依存するモジュールを入れる
-// この場合は、'ngMessages'というモジュールに依存してるよ!!とAngularJSに教えている
+// 一つの配列でバラバラの型が入っているものは珍しくない
+let things = [1, '2', function () {
+    alert('アラート!');
+}];
 
-myApp.controller('mainController', function ($scope, $log, $filter, $resource) {
-    console.log($resource);
-});
+things[2]();
