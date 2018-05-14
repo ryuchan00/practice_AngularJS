0a151921b6850d071092670a47c0cbc919f1967f
ryuchun00 <leokun0210@gmail.com>
2018-05-14 22:52:57 +0900

    $logを色々試した!ˆ

--- udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
+++ udemy_AngularJS/Code-functionとstringを理解しよう/Starter/app.js
@@ -1,6 +1,9 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', function ($scope, $log) {
-    console.log($log);
-    console.log($scope);
+    console.log('logのテスト');
+    console.debug('debugテスト');
+    console.info('infoのテスト');
+    console.warn('warn!!!のテスト');
+    console.error('例外のテスト');
 });
