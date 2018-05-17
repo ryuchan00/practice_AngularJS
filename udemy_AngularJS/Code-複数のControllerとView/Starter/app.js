628517f8561bdf6acf7819e5bebb1ab30256a2e0
ryuchan00 <leokun0210@gmail.com>
2018-05-17 13:55:58 +0900

    フラグメント識別子が変わることでSPAが動くんだな(#/bookmark/1)みたいな

--- udemy_AngularJS/Code-複数のControllerとView/Starter/app.js
+++ udemy_AngularJS/Code-複数のControllerとView/Starter/app.js
@@ -2,18 +2,18 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', function ($scope) {
 
-    $scope.alertClick = function() {
-      
+    $scope.alertClick = function () {
+
         alert("クリックしました");
-        
+
     };
-    
+
     $scope.name = 'Ken Fukuyama';
 }]);
 
 myApp.controller('secondController', ['$scope', function ($scope) {
 
-    $scope.alertClick = function() {
+    $scope.alertClick = function () {
 
         alert("クリックしました");
 
@@ -21,3 +21,21 @@ myApp.controller('secondController', ['$scope', function ($scope) {
 
     $scope.name = 'Ryo Sakazaki';
 }]);
+
+// ちなみにidで定義してあるハッシュがなくても、イベントは発火する
+// フラグメント識別子が変わることでSPAが動くんだな
+window.addEventListener('hashchange', function () {
+    console.log('ハッシュが変わりました' + window.location.hash);
+
+    if (window.location.hash === '#/bookmark/1') {
+        console.log('ページ1にきました')
+    }
+
+    if (window.location.hash === '#/bookmark/2') {
+        console.log('ページ2にきました')
+    }
+
+    if (window.location.hash === '#/bookmark/3') {
+        console.log('ページ3にきました')
+    }
+});
