553b91797bd8fb9ed03bb7462abeebb9779b3947
ryuchun00 <leokun0210@gmail.com>
2018-05-19 16:13:20 +0900

    spaのrouteingをモリモリやった

--- udemy_AngularJS/Code-Routing_Template_そしてControllerその２/Starter/app.js
+++ udemy_AngularJS/Code-Routing_Template_そしてControllerその２/Starter/app.js
@@ -1,29 +1,35 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function($routeProvider) {
-    
+myApp.config(function ($routeProvider) {
+
     $routeProvider
-    
-    .when('/', {
-        templateUrl: 'pages/main.html',  
-        controller: 'mainController'
-    })
-    
-    .when('/second', {
-        templateUrl: 'pages/second.html',
-        controller: 'secondController'
-    })
-    
+
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
 });
 
 myApp.controller('mainController', ['$scope', '$log', function ($scope, $log) {
 
     $scope.name = 'Main';
-    
+
 }]);
 
-myApp.controller('secondController', ['$scope', '$log', function ($scope, $log) {
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function ($scope, $log, $routeParams) {
 
-    $scope.name = 'Second';
-    
+    console.log($routeParams.num);
+    $scope.num = $routeParams.num || 'hoge';
+
 }]);
