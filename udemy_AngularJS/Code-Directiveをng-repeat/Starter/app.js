2046a7fb3cd10d9c0dcb71e626d1985f288865c4
ryuchun00 <leokun0210@gmail.com>
2018-05-20 18:24:15 +0900

    ディレクティブを組み合わせた

---
+++ udemy_AngularJS/Code-Directiveをng-repeat/Starter/app.js
@@ -0,0 +1,67 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+});
+
+myApp.controller('mainController', ['$scope', '$log', function ($scope, $log) {
+
+    $scope.people = [
+        {
+            name: '山田 太郎',
+            prefecture: '愛媛県',
+            city: '松山市',
+            address: '由利島'
+        },
+        {
+            name: '山田 二郎',
+            prefecture: '愛媛県',
+            city: '松山市',
+            address: '由利島'
+        },
+        {
+            name: '山田 三郎',
+            prefecture: '愛媛県',
+            city: '松山市',
+            address: '由利島'
+        }
+    ]
+    ;
+
+    $scope.formattedAddress = function (person) {
+        return person.prefecture + person.city + person.address;
+    };
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function ($scope, $log, $routeParams) {
+}]);
+
+myApp.directive('searchResult', function () {
+    return {
+        restrict: 'AE',
+        templateUrl: 'directives/searchresult.html',
+        replace: true,
+        scope: {
+            personObject: '=',
+            formattedAddressFunction: '&'
+        }
+    };
+});
